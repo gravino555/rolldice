7467cc04c47f8fbca4f8e9e27199a2777519f1d6
gravino555 <hockey_goals@hotmail.com>
2023-11-15 17:49:05 -0500

    message

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rolldice/gradient_container.dart';
+
+void main() {
+  runApp(
+     const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer([Colors.deepPurple,Colors.pinkAccent]),
+      ),
+    ),
+  );
+}
+
